ceec954a0a1e21f9ea003ac06858b8cf95ab7123
AlbertoTA <ajimenez@tax.org>
2013-11-26 11:51:36 -0500

    test
    Test1

--- src/Tester.java
+++ src/Tester.java
@@ -13,6 +13,7 @@ private checkParams(){
 
 System.out.println("this checks our parameters");
 System.out.println("This is alberto making a change");
+System.out.println("This is alberto making a second change");
 }
 
 }
